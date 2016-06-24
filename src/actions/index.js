708012ab144b6d85cd8c3ea3aaa377e7c7d27dfa
Ilker Sarac <ilker.sarac@t2.com.tr>
2016-06-24 16:52:45 +0300

    action ve promiseler gozlendi.

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = 'a4bc8b81aaac7f66223ce3b431694832';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?q=London,uk&appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},tr`;
+  const request = axios.get(url);
+
+  console.log('Request:', request);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
